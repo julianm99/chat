f3ef80e2521e0d59ee95ec80b29be2283bacbbbb
JulianMartinAlonso <julianmalonso99@gmail.com>
2022-10-03 18:52:16 -0300

    segundo commit

--- static_chat/codigo.js
+++ static_chat/codigo.js
@@ -1,31 +1,96 @@
-$(document).on('click', '.panel-heading span.icon_minim', function (e) {
-    var $this = $(this);
-    if (!$this.hasClass('panel-collapsed')) {
-        $this.parents('.panel').find('.panel-body').slideUp();
-        $this.addClass('panel-collapsed');
-        $this.removeClass('glyphicon-minus').addClass('glyphicon-plus');
-    } else {
-        $this.parents('.panel').find('.panel-body').slideDown();
-        $this.removeClass('panel-collapsed');
-        $this.removeClass('glyphicon-plus').addClass('glyphicon-minus');
-    }
-});
-$(document).on('focus', '.panel-footer input.chat_input', function (e) {
-    var $this = $(this);
-    if ($('#minim_chat_window').hasClass('panel-collapsed')) {
-        $this.parents('.panel').find('.panel-body').slideDown();
-        $('#minim_chat_window').removeClass('panel-collapsed');
-        $('#minim_chat_window').removeClass('glyphicon-plus').addClass('glyphicon-minus');
-    }
-});
-$(document).on('click', '#new_chat', function (e) {
-    var size = $( ".chat-window:last-child" ).css("margin-left");
-     size_total = parseInt(size) + 400;
-    alert(size_total);
-    var clone = $( "#chat_window_1" ).clone().appendTo( ".container" );
-    clone.css("margin-left", size_total);
-});
-$(document).on('click', '.icon_close', function (e) {
-    //$(this).parent().parent().parent().parent().remove();
-    $( "#chat_window_1" ).remove();
+/**
+ *
+ * jquery.charcounter.js version 1.2
+ * requires jQuery version 1.2 or higher
+ * Copyright (c) 2007 Tom Deater (http://www.tomdeater.com)
+ * Licensed under the MIT License:
+ * http://www.opensource.org/licenses/mit-license.php
+ * 
+ */
+ 
+ (function($) {
+    /**
+	 * attaches a character counter to each textarea element in the jQuery object
+	 * usage: $("#myTextArea").charCounter(max, settings);
+	 */
+	
+	$.fn.charCounter = function (max, settings) {
+		max = max || 100;
+		settings = $.extend({
+			container: "<span></span>",
+			classname: "charcounter",
+			format: "(%1 characters remaining)",
+			pulse: true,
+			delay: 0
+		}, settings);
+		var p, timeout;
+		
+		function count(el, container) {
+			el = $(el);
+			if (el.val().length > max) {
+			    el.val(el.val().substring(0, max));
+			    if (settings.pulse && !p) {
+			    	pulse(container, true);
+			    };
+			};
+			if (settings.delay > 0) {
+				if (timeout) {
+					window.clearTimeout(timeout);
+				}
+				timeout = window.setTimeout(function () {
+					container.html(settings.format.replace(/%1/, (max - el.val().length)));
+				}, settings.delay);
+			} else {
+				container.html(settings.format.replace(/%1/, (max - el.val().length)));
+			}
+		};
+		
+		function pulse(el, again) {
+			if (p) {
+				window.clearTimeout(p);
+				p = null;
+			};
+			el.animate({ opacity: 0.1 }, 100, function () {
+				$(this).animate({ opacity: 1.0 }, 100);
+			});
+			if (again) {
+				p = window.setTimeout(function () { pulse(el) }, 200);
+			};
+		};
+		
+		return this.each(function () {
+			var container;
+			if (!settings.container.match(/^<.+>$/)) {
+				// use existing element to hold counter message
+				container = $(settings.container);
+			} else {
+				// append element to hold counter message (clean up old element first)
+				$(this).next("." + settings.classname).remove();
+				container = $(settings.container)
+								.insertAfter(this)
+								.addClass(settings.classname);
+			}
+			$(this)
+				.unbind(".charCounter")
+				.bind("keydown.charCounter", function () { count(this, container); })
+				.bind("keypress.charCounter", function () { count(this, container); })
+				.bind("keyup.charCounter", function () { count(this, container); })
+				.bind("focus.charCounter", function () { count(this, container); })
+				.bind("mouseover.charCounter", function () { count(this, container); })
+				.bind("mouseout.charCounter", function () { count(this, container); })
+				.bind("paste.charCounter", function () { 
+					var me = this;
+					setTimeout(function () { count(me, container); }, 10);
+				});
+			if (this.addEventListener) {
+				this.addEventListener('input', function () { count(this, container); }, false);
+			};
+			count(this, container);
+		});
+	};
+
+})(jQuery);
+
+$(function() {
+    $(".counted").charCounter(320,{container: "#counter"});
 });
